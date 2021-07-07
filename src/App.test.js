61ee02cf68439f846a079ac3bda8a9734bff54e2
Josh Jacobson <joshjacobson14@gmail.com>
2021-07-06 18:25:20 -0700

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,9 @@
 import React from "react";
 import { screen, render, fireEvent, wait, waitFor } from "@testing-library/react";
 import App from "./App";
+
 import mockFetchMissions from "./api/fetchMissions";
+jest.mock('./api/fetchMissions');
 
 test("App renders without errors", () => {
     render(<App />)
