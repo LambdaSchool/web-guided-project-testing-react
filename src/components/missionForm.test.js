53ae9f1c45e16f154fd73449f040fdd3b947c2ba
Josh Jacobson <joshjacobson14@gmail.com>
2021-07-06 17:03:54 -0700

    lecture work-in-progress

---
+++ src/components/missionForm.test.js
@@ -0,0 +1,21 @@
+test("MissionForm component renders with a Get Data button", () => {
+    // Arrange
+    // render the component, look for a button element with the text /get data/i
+
+
+
+    // (Act)
+
+    // (Assert)
+});
+
+test("While fetching data, MissionForm component renders a loading message", () => {
+    // Arrange
+    // render the component, look for a button element with the text /get data/i
+
+
+
+    // (Act)
+
+    // (Assert)
+})
