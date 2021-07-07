37e3e0c392c6bf220b570bd6f030275492bd10cf
Josh Jacobson <joshjacobson14@gmail.com>
2021-07-06 17:30:54 -0700

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -1,3 +1,19 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
 import MissionsList from "./MissionsList";
+
+test("MissionsList component renders nothing when missions array is empty", () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+});
+
+test("MissionsList component shows mission titles when rerendered with data from the API", () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+});
