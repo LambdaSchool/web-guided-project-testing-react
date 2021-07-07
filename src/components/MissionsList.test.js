05e17257c9ca7b43b65e98486fceead4afdb80d9
Josh Jacobson <joshjacobson14@gmail.com>
2021-07-06 17:50:14 -0700

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -92,6 +92,10 @@ test("MissionsList component shows mission titles when rerendered with data from
     // Act (simulate a successful api call)
     rerender(<MissionsList missions={data} />)
     const missions = screen.getAllByTestId("mission");
+    const mission1 = screen.getByText(/telstar/i);
+    const mission2 = screen.getByText(/iri8dium next/i);
+    const mission3 = screen.getByText(/asiasat/i);
+    const mission4 = screen.getByText(/eutelsat/i);
 
     // Assert
     expect(missions).toHaveLength(4);
