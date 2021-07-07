438858b49fb282c8d82b0f9ed5a5c80193bfedcc
Josh Jacobson <joshjacobson14@gmail.com>
2021-07-06 18:16:35 -0700

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,8 @@
 // Top-level integration test for our app
+import React from "react";
+import { screen, render, fireEvent } from "@testing-library/react";
+import App from "./App";
+
+test("App renders without errors", () => {
+    render(<App />)
+});
